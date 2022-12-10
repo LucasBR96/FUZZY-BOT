35b75364f97f6df31ab265496124198ff1bdc888
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2022-12-10 09:49:56 -0300

    collision werks

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 
 from src.plane import plane
 from src.env import env
+from src.simmu import simu
 
 import numpy as np
 import sys
@@ -19,10 +20,10 @@ pl = plane(
     np.array([ SCREEN_W/2 , SCREEN_H/2 ]),
     0
 )
-
 sqr = env( 0.75*np.array([ SCREEN_W , SCREEN_H ]) )
+simm = simu( pl , sqr )
 
-dt = 1/30
+dt = 1/45
 if __name__ == "__main__":
     # keep game running till running is true
     while True:
@@ -64,5 +65,7 @@ if __name__ == "__main__":
         sqr_points = sqr.get_edges()
         pygame.draw.lines( DISPLAY_SURF , BOX_COLOR , True , sqr_points, width = 2 )
 
+
+        simm.collide()
         # Update our window
         pygame.display.update()
